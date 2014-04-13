d7fcbb1d582e738fbbe5884751191d6d31f22126
John Burrin <jburrin@localhost.localdomain>
2014-04-13 11:36:51 +0100

    Fixed the type of t, int wasn't big enough now unsigned long. Added two LED's one to come on when Air temp < 3 and the other when water temp > 85

--- BikeTemp/BikeTemp.ino
+++ BikeTemp/BikeTemp.ino
@@ -5,6 +5,9 @@
 // Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS 10
 
+// Define 2 pins to use as outputs for waring LEDs
+int led[2] = {8,9};
+
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
@@ -12,7 +15,8 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 int state = 0;
 int interval = 5000;
-int t = 0;
+long t = 0;
+float temp;
 
 
 char* strarray[] = {"Water = ", "Air   = "};
@@ -23,34 +27,39 @@ void setup(void)
 {
   // start serial port
   Serial.begin(9600);
+  pinMode(led[0], OUTPUT); 
+  pinMode(led[1], OUTPUT);
+  
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.setCursor(0,0);
   lcd.print("Ready");
-  delay(2000);
+  
   // Start up the library
   sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
 }
 
 
 void loop(void){
-    if( millis() > (interval + t)){
-        if(state == 0){
-            state = 1;
-        }else{
-            state = 0;
-        }
-        t = millis();
+// By do it this way the temperatures are aways updated and
+// and no delays are used
+  if( millis() > (interval + t)){
+    if(state == 0){
+      state = 1;
+    }else{
+      state = 0;
     }
-    Serial.println(t);
-    sensors.requestTemperatures(); // Send the command to get temperatures   
-    lcd.setCursor(0,0);
-    lcd.print(strarray[state]);
-    lcd.setCursor(0,1);
-    lcd.print(sensors.getTempCByIndex(state));
-    lcd.print((char)223);
-    lcd.print("C"); 
-
+  t = millis();
 }
+  sensors.requestTemperatures(); // Send the command to get temperatures   
+  lcd.setCursor(0,0);
+  lcd.print(strarray[state]);
+  lcd.setCursor(0,1);
+  temp = sensors.getTempCByIndex(state);
+  lcd.print(temp);
+  lcd.print((char)223);
+  lcd.print("C"); 
+}
+
 
